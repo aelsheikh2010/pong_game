c88c66460b481bf1a8f3f80296f50849ba5df1eb
Ahmed Elsheikh <golf17@gmail.com>
2023-02-05 23:12:28 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+
+screen.exitonclick()
